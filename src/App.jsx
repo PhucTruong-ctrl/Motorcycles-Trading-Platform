f2cd8ddf1d5c7ac77beca127e1cb746db3759a8c
PhucTruong <bfbdarkness498@gmail.com>
2025-03-09 17:43:39 +0700

    adding header

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Header from "./components/Header";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="my-[15px] mx-[25px]">
+      <header>
+        <Header></Header>
+      </header>
+      <div className="banner">
+        <h1>GEAR UP</h1> <br />
+        <h1 className="text-gradient">RIDE ON</h1>
       </div>
-      <h1 className='text-red-500'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </main>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between items-center self-stretch">
+      <div className="flex justify-center items-center gap-[5px]">
+        <img src="img/black-logo.svg" alt="" />
+        <div className="font-bold text-[36px]">RevNow</div>
+      </div>
+
+      <div className="flex items-center gap-[30px]">
+        <button className="text-[24px]">Browse All</button>
+        <button className="text-[24px]">Sell My Motorcycle</button>
+        <button className="text-[24px]">About Us</button>
+        <button className="text-[24px]">Sign In</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
