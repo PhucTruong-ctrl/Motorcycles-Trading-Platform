2d2db4a543d045b28b7a105f31bc3342fd915e86
PhucTruong <bfbdarkness498@gmail.com>
2025-03-22 21:48:27 +0700

    allow the user to upload their own avatar and banner

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -21,6 +21,7 @@ const Profile = () => {
   const [error, setError] = useState(null);
   const [atListing, setAtListing] = useState(true);
   const [repType, setRepType] = useState(null);
+  const [alreadyRep, setAlreadyRep] = useState(false);
   const [repMessage, setRepMessage] = useState("");
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
@@ -65,6 +66,23 @@ const Profile = () => {
 
         if (repError) throw repError;
         setRep(repData || []);
+
+        const { data: existingRep, error: checkError } = await supabase
+          .from("REPUTATION")
+          .select("*")
+          .eq("uid_rep", uid)
+          .eq("uid_send_rep", currentUser.id)
+          .maybeSingle();
+
+        if (checkError && checkError.code !== "PGRST116") {
+          throw checkError;
+        }
+
+        if (existingRep) {
+          setAlreadyRep(true);
+        } else {
+          setAlreadyRep(false);
+        }
       } catch (error) {
         console.error("Error fetching data:", error);
         setError(error);
@@ -78,11 +96,9 @@ const Profile = () => {
 
   const ListingButton = () => {
     setAtListing(true);
-    console.log("Listing button: ", user.uid);
   };
   const ReputationButton = () => {
     setAtListing(false);
-    console.log("Reputation button: ", user.uid);
   };
 
   const sendRep = async (e) => {
@@ -96,60 +112,139 @@ const Profile = () => {
     }
 
     try {
-      const { data: existingRep, error: checkError } = await supabase
-        .from("REPUTATION")
-        .select("*")
-        .eq("uid_rep", uid)
-        .eq("uid_send_rep", currentUser.id)
-        .maybeSingle();
+      const { rep_data, rep_error } = await supabase.from("REPUTATION").insert([
+        {
+          uid_rep: uid,
+          uid_send_rep: currentUser.id,
+          rep_message: repMessage,
+          is_positive_rep: repType,
+        },
+      ]);
 
-      if (checkError && checkError.code !== "PGRST116") {
-        throw checkError;
-      }
+      console.log("Reputation Data: ", rep_data);
 
-      if (existingRep) {
-        alert("You have already sent a reputation to this user.");
-        return;
-      } else {
-        const { rep_data, rep_error } = await supabase
-          .from("REPUTATION")
-          .insert([
-            {
-              uid_rep: uid,
-              uid_send_rep: currentUser.id,
-              rep_message: repMessage,
-              is_positive_rep: repType,
-            },
-          ]);
-        if (rep_error) throw rep_error;
+      if (rep_error) throw rep_error;
 
-        if (repType) {
-          const { user_data, user_error } = await supabase
-            .from("USER")
-            .update({ reputation: user.reputation + 1 })
-            .eq("uid", uid)
-            .select();
+      if (repType) {
+        const { user_data, user_error } = await supabase
+          .from("USER")
+          .update({ reputation: user.reputation + 1 })
+          .eq("uid", uid)
+          .select();
 
-          if (user_error) {
-            console.error("Error updating rep:", error);
-            return;
-          }
-        } else {
-          const { user_data, user_error } = await supabase
-            .from("USER")
-            .update({ bad_rep: user.bad_rep + 1 })
-            .eq("uid", uid)
-            .select();
+        console.log("User Data: ", user_data);
 
-          if (user_error) {
-            console.error("Error updating rep:", error);
-            return;
-          }
+        if (user_error) {
+          console.error("Error updating rep:", error);
+          return;
         }
-        alert("Rep sended");
+      } else {
+        const { user_data, user_error } = await supabase
+          .from("USER")
+          .update({ bad_rep: user.bad_rep + 1 })
+          .eq("uid", uid)
+          .select();
+
+        console.log("User Data: ", user_data);
+
+        if (user_error) {
+          console.error("Error updating rep:", error);
+          return;
+        }
       }
+      alert("Rep sended");
     } catch (error) {
       console.error("Rep send error", error);
+    }
+  };
+
+  const handleAvatarUpload = async (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    try {
+      const fileName = `avatar_${Date.now()}_${file.name}`;
+
+      const { data: uploadData, error: uploadError } = await supabase.storage
+        .from("user-media")
+        .upload(`${currentUser.id}/${fileName}`, file);
+
+      if (uploadError) throw uploadError;
+
+      const { data: publicUrlData } = supabase.storage
+        .from("user-media")
+        .getPublicUrl(`${currentUser.id}/${fileName}`);
+
+      const newAvatarUrl = publicUrlData.publicUrl;
+
+      if (user?.avatar_url) {
+        const oldFileName = user.avatar_url.split("/").pop();
+        const { error: deleteError } = await supabase.storage
+          .from("user-media")
+          .remove([`${currentUser.id}/${oldFileName}`]);
+
+        if (deleteError) throw deleteError;
+      }
+
+      const { data: userData, error: userError } = await supabase
+        .from("USER")
+        .update({ avatar_url: newAvatarUrl })
+        .eq("uid", uid)
+        .select();
+
+      if (userError) throw userError;
+
+      setUser({ ...user, avatar_url: newAvatarUrl });
+
+      alert("Avatar updated successfully!");
+    } catch (error) {
+      console.error("Error updating avatar:", error);
+      alert("Failed to update avatar.");
+    }
+  };
+
+  const handleBannerUpload = async (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    try {
+      const fileName = `banner_${Date.now()}_${file.name}`;
+
+      const { data: uploadData, error: uploadError } = await supabase.storage
+        .from("user-media")
+        .upload(`${currentUser.id}/${fileName}`, file);
+
+      if (uploadError) throw uploadError;
+
+      const { data: publicUrlData } = supabase.storage
+        .from("user-media")
+        .getPublicUrl(`${currentUser.id}/${fileName}`);
+
+      const newBannerUrl = publicUrlData.publicUrl;
+
+      if (user?.banner_url) {
+        const oldFileName = user.avatar_url.split("/").pop();
+        const { error: deleteError } = await supabase.storage
+          .from("user-media")
+          .remove([`${currentUser.id}/${oldFileName}`]);
+
+        if (deleteError) throw deleteError;
+      }
+
+      const { data: userData, error: userError } = await supabase
+        .from("USER")
+        .update({ banner_url: newBannerUrl })
+        .eq("uid", uid)
+        .select();
+
+      if (userError) throw userError;
+
+      setUser({ ...user, banner_url: newBannerUrl });
+
+      alert("Banner updated successfully!");
+    } catch (error) {
+      console.error("Error updating Banner:", error);
+      alert("Failed to update Banner.");
     }
   };
 
@@ -168,26 +263,66 @@ const Profile = () => {
           <Header />
         </header>
         <div className="flex flex-col gap-4">
-          <div className="bg-red min-h-50 rounded-xl h-[265px] overflow-hidden">
+          <div className="relative rounded-xl w-full h-[270px] overflow-hidden flex justify-center items-center">
             <img
-              src="/img/bannerDefault.png"
+              src={user?.banner_url}
               alt=""
-              className="object-cover w-full"
+              className="object-cover w-full h-full"
             />
+            {currentUser?.id === uid && (
+              <div className="absolute">
+                <label htmlFor="avt-upload">
+                  <img
+                    src="/icons/Upload.svg"
+                    alt=""
+                    className="w-30 opacity-0 hover:opacity-70 bg-white rounded-md cursor-pointer transition"
+                  />
+                </label>
+                <input
+                  id="avt-upload"
+                  type="file"
+                  accept="image/*"
+                  className="hidden"
+                  onChange={handleBannerUpload}
+                />
+              </div>
+            )}
           </div>
 
           <div className="flex flex-col md:flex-row gap-6 w-full bg-white rounded-xl shadow-md relative p-5">
-            <img
-              src={user?.avatar_url}
-              alt=""
-              className="w-[150px] h-[150px] rounded-xl border-2 border-grey"
-            />
+            <div className="relative flex justify-center items-center">
+              <img
+                src={user?.avatar_url}
+                alt=""
+                className="w-[150px] h-[150px] rounded-xl border-2 border-grey"
+              />
+              {currentUser?.id === uid && (
+                <div className="absolute">
+                  <label htmlFor="avt-upload">
+                    <img
+                      src="/icons/Upload.svg"
+                      alt=""
+                      className="w-15 opacity-0 hover:opacity-70 bg-white rounded-md cursor-pointer transition"
+                    />
+                  </label>
+                  <input
+                    id="avt-upload"
+                    type="file"
+                    accept="image/*"
+                    className="hidden"
+                    onChange={handleAvatarUpload}
+                  />
+                </div>
+              )}
+            </div>
             <div className="flex flex-row gap-6 justify-center items-center">
               <div className="flex flex-col gap-2.5">
                 <h2>{user?.name}</h2>
                 <div className="flex flex-row gap-2 justify-start items-center">
                   <img src="/icons/Location.svg" alt="" className="w-2.5" />
-                  <div className="font-light text-[16px]">{user?.address}</div>
+                  <div className="font-light text-[16px]">
+                    {user?.state}, {user?.city}
+                  </div>
                 </div>
               </div>
               <div className="flex flex-col gap-2.5">
@@ -221,13 +356,13 @@ const Profile = () => {
 
           <div className="bg-white w-full h-20 rounded-xl shadow-md flex flex-row gap-12 justify-start items-center p-5">
             <button
-              className="font-bold text-[28px] text-blue"
+              className={`font-bold text-[28px] text-blue ${atListing ? "border-b-2" : "border-b-0"}`}
               onClick={ListingButton}
             >
               Listing
             </button>
             <button
-              className="font-bold text-[28px] text-blue"
+              className={`font-bold text-[28px] text-blue ${atListing === false ? "border-b-2" : "border-b-0"}`}
               onClick={ReputationButton}
             >
               Reputation
@@ -244,7 +379,7 @@ const Profile = () => {
             </div>
           ) : (
             <div className="w-full">
-              {currentUser.id !== uid && (
+              {currentUser.id !== uid && alreadyRep === false && (
                 <form
                   onSubmit={sendRep}
                   className="flex flex-col gap-3 items-center justify-center bg-white shadow-md p-10 rounded-[6px]"
