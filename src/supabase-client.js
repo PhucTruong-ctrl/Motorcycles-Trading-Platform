fe452c4018352c45aea801c9b998c3fb6ed466ca
PhucTruong <bfbdarkness498@gmail.com>
2025-03-24 14:12:21 +0700

    fixing typo

--- src/supabase-client.js
+++ src/supabase-client.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseURL = import.meta.env.VITE_SUPABASE_URL;
-const supbaseAnonKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY;
 
-const supabase = createClient(supabaseURL, supbaseAnonKey);
+const supabase = createClient(supabaseURL, supabaseAnonKey);
 
 export default supabase;
