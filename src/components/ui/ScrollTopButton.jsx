7310a4fbd8ad094df44ce07adcd87ba9144af60c
PhucTruong <bfbdarkness498@gmail.com>
2025-05-02 14:18:50 +0700

    Update ScrollTopButton.jsx

--- src/components/ui/ScrollTopButton.jsx
+++ src/components/ui/ScrollTopButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ScrollTopButton = () => {
   const handleScrollTop = () => window.scrollTo({ top: 0, behavior: "smooth" });
   return (
-    <button className="fixed w-10 h-10 rounded-full bg-black bottom-3 left-3 flex justify-center items-center z-99 hover:scale-110 transition" onClick={handleScrollTop}>
+    <button className="fixed w-10 h-10 rounded-full bg-black opacity-90 bottom-3 left-3 flex justify-center items-center z-99 hover:scale-110 transition" onClick={handleScrollTop}>
       <img src="/icons/ArrowUpward.svg" alt="" />
     </button>
   );
